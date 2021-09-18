8fc400e39837c921041c550624e0b7b60e0a2a6b
d-laslo <ice_bear@ukr.net>
2021-09-19 01:48:43 +0300

    add summing and transposition operations

--- headers/matrix.hpp
+++ headers/matrix.hpp
@@ -2,6 +2,7 @@
 #include "defines.hpp"
 #include "source.hpp"
 #include <cstdint>
+#include <algorithm>
 
 
 using type = uint64_t;
@@ -12,10 +13,17 @@ private:
     type** matrix;
     uint64_t num_row;
     uint64_t num_column;
+
+    type** matrix_init();
 public:
     Matrix();
     Matrix(uint64_t size);
     Matrix(uint64_t num_row, uint64_t num_column);
     Matrix(const Matrix& other);
     //Matrix(Matrix&& other);
+
+    //transpose the matrix
+    Matrix& T();
+
+    Matrix& operator+(const Matrix& other);
 };
--- source/matrix.cpp
+++ source/matrix.cpp
@@ -7,18 +7,12 @@ Matrix::Matrix(): num_row(0), num_column(0)
 
 Matrix::Matrix(uint64_t size): num_row(size), num_column(size) 
 {
-    matrix = new type*[num_row];
-    for (uint64_t i = 0; i < num_row; i++) {
-        matrix[i] = new type[num_column];
-    }
+    matrix = matrix_init();
 }
 
 Matrix::Matrix(uint64_t num_row, uint64_t num_column): num_row(num_row), num_column(num_column) 
 {
-    matrix = new type*[num_row];
-    for (uint64_t i = 0; i < num_row; i++) {
-        matrix[i] = new type[num_column];
-    }
+    matrix = matrix_init();
 }
 
 Matrix::Matrix(const Matrix& other)
@@ -26,11 +20,48 @@ Matrix::Matrix(const Matrix& other)
     this->num_row = other.num_row;
     this->num_column = other.num_column;
     matrix = new type*[num_row];
-    
+
     for (uint64_t i = 0; i < num_row; i++) {
         matrix[i] = new type[num_column];
         for (uint64_t j = 0; j < num_column; j++) {
             matrix[i][j] = other.matrix[i][j];
         }
     }
+}
+
+type** Matrix::matrix_init()
+{
+    type** new_matrix = new type*[num_row];
+    for (uint64_t i = 0; i < num_row; i++) {
+        matrix[i] = new type[num_column];
+    }
+    return new_matrix;
+}
+
+Matrix& Matrix::T()
+{
+    Matrix* transpose_matrix = new Matrix(num_column, num_row);
+
+    for (uint64_t i = 0; i < num_row; i++) {
+        for (uint64_t j = 0; j < num_column; j++) {
+            transpose_matrix->matrix[j][i] = matrix[i][j];
+        }
+    }
+    return *transpose_matrix;
+}
+
+Matrix& Matrix::operator+(const Matrix& other)
+{
+    if ( (this->num_row != other.num_row) || (this->num_column != other.num_column) ) {
+        throw -1;
+    }
+
+    Matrix* result = new Matrix(num_row, num_column);
+    for (uint64_t i = 0; i < num_row; i++) {
+        for (uint64_t j = 0; j < num_column; j++) {
+            result->matrix[i][j] = this->matrix[i][j] + other.matrix[i][j];
+        }
+    }
+
+    return *result;
 }
