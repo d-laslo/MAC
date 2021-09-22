4a3cb96d41b2476a18c225cc15201024e53abb0e
d-laslo <ice_bear@ukr.net>
2021-09-22 21:16:21 +0300

    add long int for equations

---
+++ headers/long_int.hpp
@@ -0,0 +1,51 @@
+#pragma once
+#include "defines.hpp"
+#include "source.hpp"
+#include <vector>
+
+// спосіб представлення Litle Endian
+class Long
+{
+private:
+    // число яке зберігаємо
+    std::vector<__uint128_t> number;
+
+    // максимальна довжина числа в бітах
+    __uint128_t max_number_length;
+
+    // наявна довжина числа в бітах
+    __uint128_t length;
+
+    // кількість зайнятих біт в останьому кластері
+    uint64_t length_last_cluster;
+
+    // максимальна кількість бітів яка поміститься в кластері
+    uint64_t max_cluster_length;
+
+    // вказує на кількість використовуваних кластерів
+    uint64_t number_of_clusters_used;
+    
+public:
+    Long();
+    Long(__uint128_t max_len);
+    Long(const Long& other);
+
+
+    Long& operator^(const Long& other) const;
+    // __uint128_t& operator[](uint64_t index);
+
+    // добавляє в кінець послідовності біт
+    void push_back(uint64_t val);
+
+    // повертає індек найстаршого біта
+    // відрахунок починається із сторони молодших бітів 
+    // індексація починається з 0
+    uint64_t hight_bit() const;
+
+
+    // Long& operator<<(__uint128_t shift) const;
+    // Long& operator>>(__uint128_t shift) const;
+
+private:
+    void check(const Long& other) const;
+};
---
+++ source/long_int.cpp
@@ -0,0 +1,88 @@
+#include "../headers/long_int.hpp"
+
+Long::Long(__uint128_t max_len) : max_number_length(max_len)
+{
+    max_cluster_length = sizeof(__uint128_t) * 8;
+    number = std::vector<__uint128_t>(max_number_length / max_cluster_length);
+
+    length = 0;
+    length_last_cluster = max_cluster_length;
+    number_of_clusters_used = 0;
+}
+
+
+Long::Long(const Long& other)
+{
+    this->max_number_length = other.max_number_length;
+    this->max_cluster_length = other.max_cluster_length;
+    this->length = other.length;
+    this->length_last_cluster = other.length_last_cluster;
+    this->number_of_clusters_used = other.number_of_clusters_used;
+    this->number = other.number;
+}
+
+
+Long& Long::operator^(const Long& other) const
+{
+    check(other);
+    Long* result = new Long(*this);
+
+    auto& res = result->number;
+
+    for (uint64_t index = 0; index < number_of_clusters_used; index++) {
+        res[index] ^= other.number[index];
+    }
+    return *result;
+}
+
+
+// __uint128_t& Long::operator[](uint64_t index)
+// {
+//     return number[index];
+// }
+
+
+void Long::push_back(uint64_t val)
+{
+    if (length_last_cluster == max_cluster_length)
+    {
+        number_of_clusters_used++;
+        length_last_cluster = 0;
+    }
+
+    number[number_of_clusters_used - 1] ^= (val & 1) << length_last_cluster;
+    length++;
+    length_last_cluster++;
+}
+
+
+uint64_t Long::hight_bit() const
+{
+    uint64_t cluster_index = -1;
+    for (const auto& cluster: number) {
+        cluster_index++;
+        for (uint64_t i = max_cluster_length - 1; (i >= 0) && (i < -1); i--) {
+            if ( (cluster >> i) & 1 ) {
+                return max_number_length - cluster_index * max_cluster_length - max_cluster_length + i;
+            }
+        }
+    }
+    return 0;
+}
+
+
+void Long::check(const Long& other) const 
+{
+    if (this->max_number_length != other.max_number_length) {
+        throw -1;
+    }
+    if (this->max_cluster_length != other.max_cluster_length) {
+        throw -2;
+    }
+    if (this->length != other.length) {
+        throw -3;
+    }
+    if (this->number_of_clusters_used != other.number_of_clusters_used) {
+        throw -4;
+    }
+}
