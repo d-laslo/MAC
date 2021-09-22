1351607e63225e0e3c4a9aa97ef78b79a39f501e
d-laslo <ice_bear@ukr.net>
2021-09-22 15:04:01 +0300

    fix

--- headers/source.hpp
+++ headers/source.hpp
@@ -6,7 +6,7 @@ template<class T>
 uint64_t high_bit(T value) 
 {
     auto hb = sizeof(value) * 8 - 1;
-    for (; hb >= 0; hb--) {
+    for (; hb >= 0 && hb < -1; hb--) {
         if ((value >> hb) & 0x1) {
             return hb + 1;
         }
