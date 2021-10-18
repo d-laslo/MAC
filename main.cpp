095f4e315395ee002b8ec69ed2a08a0f4a500559
d-laslo <ice_bear@ukr.net>
2021-10-18 04:28:02 +0300

    add main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include "headers/defines.hpp"
 #include "headers/source.hpp"
+#include "headers/goppa.hpp"
+#include "headers/mcellice.hpp"
+#include <vector>
+#include <array>
+#include <bitset>
+
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    
+    McEllice tmp(7, 19, 12);
+    tmp.test();
+    // Long tmp(500);
+    // tmp.test();
     return 0;
 }
